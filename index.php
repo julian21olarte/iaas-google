dd9df8bc48dabb00c82151a13a8c3bbede8d79a9
julian21olarte <julian21olarte@gmail.com>
2018-05-08 12:49:21 -0500

    first commit;

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+  if(isset($_POST['login'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    login($username, $password);
+  }
+  function renderIndex()
+  {
+    $text = file_get_contents("index.html");
+    echo $text;
+  }
+
+  function login($username, $password) {
+    $mysqli = new mysqli("localhost", "root", "root", "iaasgoogle", 3306);
+    if ($mysqli->connect_errno) {
+        echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    }
+    $user = $mysqli->query("SELECT * FROM users WHERE username = '$username' AND password = '$password' LIMIT 1");
+    if($user->num_rows>0) {
+      echo 'Usuario loggueado correctamente!';
+    }
+    else {
+      echo 'No existe un usuario con esas credenciales!';
+    }
+  }
+
+  renderIndex();
+?>
